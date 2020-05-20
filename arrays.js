bfa10534f0189ed942770307413a3c8cb2c8a475
Stephanie Shih <stephk.shih@gmail.com>
2020-05-19 17:01:31 -0700

    Translated functions in arrays.js to JavaScript code

--- arrays.js
+++ arrays.js
@@ -2,18 +2,43 @@
 
 
 // 1. printIndices
+//
+// Print each item in the list, followed by its index.
+//
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`);
+  }
 }
 
 
 // 2. everyOtherItem
+//
+// Print an array of every other item in a given array, starting with index 0.
+//
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+        result.push(items[i]);
+    }
+  }
+
+  console.log(result);
 }
 
 
 // 3. smallestNItems
+//
+// Print a list of the 'n' smallest integers in 'items'.
+// Order the integers in descending order.
+// You can assume that 'n' will be less than the length of the list.
+//
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  const sortedNItems = sortedItems.slice(0, n);
+  sortedNItems.reverse();
+
+  console.log(sortedNItems);
 }
