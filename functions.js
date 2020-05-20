7b463bb291bc9f6ca1ef9428c4a05c58a6d8edef
Stephanie Shih <stephk.shih@gmail.com>
2020-05-19 17:18:01 -0700

    Translated functions in functions.js to JavaScript code

--- functions.js
+++ functions.js
@@ -2,15 +2,45 @@
 
 
 // 1. isHometown
-
-// Define your function here
+//
+// Given a town, return true if town is San Francisco.
+//
+function isHometown(town) {
+    return town === 'San Francisco';
+}
 
 
 // 2. getFullName
-
-// Define your function here
+//
+// Given a first and last name, return a full name.
+//
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
 
 
 // 3. calculateTotal
+//
+// Return the total price of an item, figuring in state taxes and fees.
+//
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
 
-// Define your function here
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA') {
+        fee = 2;
+    }
+    else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        }
+        else {
+            fee = 3;
+        }
+    }
+
+    return subtotal + fee;
+}
