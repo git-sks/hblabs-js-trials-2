0ecf7927c777f3712fd123b135c37eb743ad6ccf
Stephanie Shih <stephk.shih@gmail.com>
2020-05-19 18:10:41 -0700

    Translated functions in objects.js to JavaScript code

--- objects.js
+++ objects.js
@@ -2,12 +2,40 @@
 
 
 // 1. countWords
+//
+//Return an object of each word and the number of times they appear.
+//
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word)) {
+        wordCounts[word] += 1;
+    }
+    else {
+        wordCounts[word] = 1;
+    }
+  }
+
+  return wordCounts;
 }
 
 
 // 2. getMelonsAtPrice
+//
+// Return the list of melons that cost the given price.
+//
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas'],
+  };
+
+  if (!melonPrices.hasOwnProperty(price)) {
+    return;
+  }
+
+  return melonPrices[price].sort();
 }
